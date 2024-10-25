71dfb62798390038458a7bbda3f49a48672c25e6
Mrash <songpol.saikhwa@gmail.com>
2024-10-26 00:03:21 +0700

    meoiw

--- modules/arrange.py
+++
@@ -1,9 +0,0 @@
-fewshots= (open('quote.txt', 'r',encoding="UTF-8").readlines())
-x = []
-for _ in fewshots:
-    if _ not in x:
-        x.append(_)
-    else:
-        pass
-print(len(x),len(fewshots))
-open('quote.txt', 'w',encoding="UTF-8").write("".join(x))
